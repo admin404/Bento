974fde702969c22122c86de66cc13fcc902df42d
admin404 <neterror404@gmail.com>
2022-04-21 15:40:31 +0300

    update

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ const CONFIG = {
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '31.805844',
+	defaultLongitude: '34.650207',
 
 	// Autochange
 	autoChangeTheme: false,
