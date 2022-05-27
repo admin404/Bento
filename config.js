d1455b1e8a989ae488838d1231a950be479906d6
admin404 <neterror404@gmail.com>
2022-05-27 21:26:35 +0300

    update 27/05/2022 21:26:34.07

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: 'c751ce7d9f0f9400b2fa6be448c859bb', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
